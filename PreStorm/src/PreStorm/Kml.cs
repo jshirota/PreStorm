1f1d7ede89df251409f51500dd26db93c17ef464
Jiro Shirota <jshirota@gmail.com>
2015-12-03 23:37:45 -0500

    kml namespace exposed

--- PreStorm/src/PreStorm/Kml.cs
+++ PreStorm/src/PreStorm/Kml.cs
@@ -10,7 +10,10 @@ namespace PreStorm
     /// </summary>
     public static class Kml
     {
-        private static readonly XNamespace kml = "http://www.opengis.net/kml/2.2";
+        /// <summary>
+        /// The XML namespace for KML.
+        /// </summary>
+        public static XNamespace ns { get; } = "http://www.opengis.net/kml/2.2";
 
         private static string ToCoordinates(this Point geometry, double? z)
         {
@@ -29,31 +32,31 @@ namespace PreStorm
 
         private static XElement ToKmlPoint(this Point geometry, double? z, XElement[] extraElements)
         {
-            return new XElement(kml + "Point", extraElements,
-                new XElement(kml + "coordinates", geometry.ToCoordinates(z)));
+            return new XElement(ns + "Point", extraElements,
+                new XElement(ns + "coordinates", geometry.ToCoordinates(z)));
         }
 
         private static XElement ToKmlMultipoint(this Multipoint geometry, double? z, XElement[] extraElements)
         {
-            return new XElement(kml + "MultiGeometry",
+            return new XElement(ns + "MultiGeometry",
                 geometry.points.Select(p => new Point { x = p[0], y = p[1], z = p.Length > 2 ? p[2] : (double?)null }.ToKmlPoint(z, extraElements)));
         }
 
         private static XElement ToKmlPolyline(this Polyline geometry, double? z, XElement[] extraElements)
         {
-            return new XElement(kml + "MultiGeometry",
+            return new XElement(ns + "MultiGeometry",
                 geometry.paths.Select(p =>
-                    new XElement(kml + "LineString", extraElements,
-                        new XElement(kml + "coordinates", p.ToCoordinates(z)))));
+                    new XElement(ns + "LineString", extraElements,
+                        new XElement(ns + "coordinates", p.ToCoordinates(z)))));
         }
 
         private static XElement ToKmlPolygon(this Polygon geometry, double? z, XElement[] extraElements)
         {
-            return new XElement(kml + "MultiGeometry",
-                geometry.GroupRings().Select(p => new XElement(kml + "Polygon", extraElements,
-                    p.Select(r => new XElement(kml + (r.IsInnerRing() ? "innerBoundaryIs" : "outerBoundaryIs"),
-                        new XElement(kml + "LinearRing",
-                            new XElement(kml + "coordinates", r.ToCoordinates(z))))))));
+            return new XElement(ns + "MultiGeometry",
+                geometry.GroupRings().Select(p => new XElement(ns + "Polygon", extraElements,
+                    p.Select(r => new XElement(ns + (r.IsInnerRing() ? "innerBoundaryIs" : "outerBoundaryIs"),
+                        new XElement(ns + "LinearRing",
+                            new XElement(ns + "coordinates", r.ToCoordinates(z))))))));
         }
 
         /// <summary>
@@ -63,16 +66,16 @@ namespace PreStorm
         /// <returns></returns>
         public static XElement ToKml(this KmlStyle style)
         {
-            return new XElement(kml + "Style", new XAttribute("id", style.GetHashCode()),
-                       new XElement(kml + "IconStyle",
-                           new XElement(kml + "color", style.IconColour),
-                           new XElement(kml + "scale", style.IconScale),
-                           new XElement(kml + "Icon", style.IconUrl)),
-                       new XElement(kml + "LineStyle",
-                           new XElement(kml + "color", style.LineColour),
-                           new XElement(kml + "width", style.LineWidth)),
-                       new XElement(kml + "PolyStyle",
-                           new XElement(kml + "color", style.PolygonColour)));
+            return new XElement(ns + "Style", new XAttribute("id", style.GetHashCode()),
+                       new XElement(ns + "IconStyle",
+                           new XElement(ns + "color", style.IconColour),
+                           new XElement(ns + "scale", style.IconScale),
+                           new XElement(ns + "Icon", style.IconUrl)),
+                       new XElement(ns + "LineStyle",
+                           new XElement(ns + "color", style.LineColour),
+                           new XElement(ns + "width", style.LineWidth)),
+                       new XElement(ns + "PolyStyle",
+                           new XElement(ns + "color", style.PolygonColour)));
         }
 
         /// <summary>
@@ -117,12 +120,12 @@ namespace PreStorm
         /// <returns></returns>
         public static XElement ToKml(this Feature feature, string name = null, double? z = null, XElement[] geometryElements = null, params XElement[] placemarkElements)
         {
-            return new XElement(kml + "Placemark", new XAttribute("id", feature.OID),
-                       new XElement(kml + "name", name), placemarkElements,
-                       new XElement(kml + "ExtendedData",
+            return new XElement(ns + "Placemark", new XAttribute("id", feature.OID),
+                       new XElement(ns + "name", name), placemarkElements,
+                       new XElement(ns + "ExtendedData",
                            from n in feature.GetFieldNames()
-                           select new XElement(kml + "Data", new XAttribute("name", n),
-                                      new XElement(kml + "value", feature[n]))),
+                           select new XElement(ns + "Data", new XAttribute("name", n),
+                                      new XElement(ns + "value", feature[n]))),
                                           feature.GetType().HasGeometry() ? ToKml(((dynamic)feature).Geometry, z, geometryElements) : null);
         }
 
@@ -150,8 +153,8 @@ namespace PreStorm
         /// <returns></returns>
         public static XElement ToKml<T>(this IEnumerable<T> features, Func<T, string> name, Func<T, double?> z, Func<T, XElement[]> placemarkElements, params XElement[] documentElements) where T : Feature
         {
-            return new XElement(kml + "kml",
-                       new XElement(kml + "Document", documentElements,
+            return new XElement(ns + "kml",
+                       new XElement(ns + "Document", documentElements,
                            features.Select(f => f.ToKml(name?.Invoke(f), z?.Invoke(f), null, placemarkElements?.Invoke(f)))));
         }
 
@@ -174,7 +177,7 @@ namespace PreStorm
             var dictionary = features.Distinct().ToDictionary(f => f, style);
 
             return dictionary.Keys.ToKml(name, z,
-                f => new[] { new XElement(kml + "styleUrl", "#" + dictionary[f].GetHashCode()) }.Concat(placemarkElements == null ? new XElement[] { } : placemarkElements(f)).ToArray(),
+                f => new[] { new XElement(ns + "styleUrl", "#" + dictionary[f].GetHashCode()) }.Concat(placemarkElements == null ? new XElement[] { } : placemarkElements(f)).ToArray(),
                 dictionary.Values.Distinct().Select(ToKml).Concat(documentElements ?? new XElement[] { }).ToArray());
         }
     }
